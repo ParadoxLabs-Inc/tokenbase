f23e56ad8709b4f40983fae0af2ad271491e1431
Ryan Hoerr <ryan@paradoxlabs.com>
2015-08-03 12:45:46 -0400

    Added admin notification feed.

---
+++ Model/Observer/AdminNotification.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Paradox Labs, Inc.
+ * http://www.paradoxlabs.com
+ * 717-431-3330
+ *
+ * Need help? Open a ticket in our support system:
+ *  http://support.paradoxlabs.com
+ *
+ * @author      Ryan Hoerr <magento@paradoxlabs.com>
+ * @license     http://store.paradoxlabs.com/license.html
+ */
+
+namespace ParadoxLabs\TokenBase\Model\Observer;
+
+/**
+ * Check for extension updates/notifications
+ */
+class AdminNotification extends \Magento\AdminNotification\Model\Observer
+{
+    /**
+     * We inject our feed factory via DI. magic.
+     */
+}
---
+++ Model/Observer/AdminNotification/Feed.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Paradox Labs, Inc.
+ * http://www.paradoxlabs.com
+ * 717-431-3330
+ *
+ * Need help? Open a ticket in our support system:
+ *  http://support.paradoxlabs.com
+ *
+ * @author      Ryan Hoerr <magento@paradoxlabs.com>
+ * @license     http://store.paradoxlabs.com/license.html
+ */
+
+namespace ParadoxLabs\TokenBase\Model\Observer\AdminNotification;
+
+/**
+ * Check for extension updates/notifications and add any to the system.
+ */
+class Feed extends \Magento\AdminNotification\Model\Feed
+{
+    /**
+     * @var \ParadoxLabs\TokenBase\Helper\Data
+     */
+    protected $helper;
+
+    /**
+     * @var \Magento\Framework\Module\ResourceInterface
+     */
+    protected $moduleResource;
+
+    /**
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Backend\App\ConfigInterface $backendConfig
+     * @param \Magento\AdminNotification\Model\InboxFactory $inboxFactory
+     * @param \Magento\Framework\HTTP\Adapter\CurlFactory $curlFactory
+     * @param \Magento\Framework\App\DeploymentConfig $deploymentConfig
+     * @param \Magento\Framework\App\ProductMetadataInterface $productMetadata
+     * @param \Magento\Framework\UrlInterface $urlBuilder
+     * @param \ParadoxLabs\TokenBase\Helper\Data $helper
+     * @param \Magento\Framework\Module\ResourceInterface $moduleResource
+     * @param \Magento\Framework\Model\Resource\AbstractResource $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Backend\App\ConfigInterface $backendConfig,
+        \Magento\AdminNotification\Model\InboxFactory $inboxFactory,
+        \Magento\Framework\HTTP\Adapter\CurlFactory $curlFactory,
+        \Magento\Framework\App\DeploymentConfig $deploymentConfig,
+        \Magento\Framework\App\ProductMetadataInterface $productMetadata,
+        \Magento\Framework\UrlInterface $urlBuilder,
+        \ParadoxLabs\TokenBase\Helper\Data $helper,
+        \Magento\Framework\Module\ResourceInterface $moduleResource,
+        \Magento\Framework\Model\Resource\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        $this->helper = $helper;
+        $this->moduleResource = $moduleResource;
+
+        parent::__construct(
+            $context,
+            $registry,
+            $backendConfig,
+            $inboxFactory,
+            $curlFactory,
+            $deploymentConfig,
+            $productMetadata,
+            $urlBuilder,
+            $resource,
+            $resourceCollection,
+            $data
+        );
+    }
+
+    /**
+     * Retrieve feed url
+     *
+     * @return string
+     */
+    public function getFeedUrl()
+    {
+        $httpPath       = $this->_backendConfig->isSetFlag(self::XML_USE_HTTPS_PATH) ? 'https://' : 'http://';
+
+        $methods        = $this->helper->getAllMethods();
+        $methods[]      = 'tokenbase';
+
+        $this->_feedUrl = $httpPath . 'store.paradoxlabs.com/updates.php?key=' . implode(',', $methods)
+            . '&version=' . (string)$this->moduleResource->getDbVersion('ParadoxLabs_TokenBase');
+
+        return $this->_feedUrl;
+    }
+}
