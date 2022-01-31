1e8f7fd1c33ae6692133c16815977b78f6199536
Ryan <ryan@paradoxlabs.com>
2022-01-31 12:46:54 -0500

    Fixed ability to use TokenBase methods for free orders. (1937307)

--- view/frontend/web/js/mixin/checkout/model/payment-service.js
+++ view/frontend/web/js/mixin/checkout/model/payment-service.js
@@ -1,20 +1,29 @@
 define([
     'underscore',
-    'Magento_Checkout/js/model/payment/method-list'
-], function (_, methodList) {
+    'Magento_Checkout/js/model/payment/method-list',
+    'mage/utils/wrapper'
+], function (_, methodList, wrapper) {
     'use strict';
 
     return function (target) {
         // Removing free check. Eligibility should be handled fine on the server side--we won't be given a
         // method that doesn't actually apply. If ZeroTotal is enabled and applicable, great.
         target.getAvailablePaymentMethods = function() {
-            var methods = [];
-            _.each(methodList(), function (method) {
-                methods.push(method);
-            });
-
-            return methods;
+            return methodList().slice();
         };
+
+        target.setPaymentMethods = wrapper.wrapSuper(target.setPaymentMethods, function(methods) {
+            var filteredMethods = methods;
+
+            if (methods && methods.length > 0) {
+                var freeMethod = _.find(methods, function (paymentMethod) {
+                    return paymentMethod.method === 'free';
+                });
+                var filteredMethods = _.without(methods, freeMethod);
+            }
+
+            this._super(filteredMethods);
+        });
 
         return target;
     };
