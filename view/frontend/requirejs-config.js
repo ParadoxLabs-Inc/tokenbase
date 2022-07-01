a8e269b0cbd3c09f9f6ae1e456ba03128ab65214
Ryan Hammett <ryan.hammett@paradoxlabs.com>
2022-07-01 15:50:46 -0400

    2206095 Remove config mixins for the removed js files for last commit (8d175cc)

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -5,15 +5,5 @@ var config = {
             tokenbaseForm: 'ParadoxLabs_TokenBase/js/form',
             tokenbaseCardFormatter: 'ParadoxLabs_TokenBase/js/cardFormatter'
         }
-    },
-    config: {
-        mixins: {
-            'Magento_Multishipping/js/payment': {
-                'ParadoxLabs_TokenBase/js/mixin/multishipping/payment': true
-            },
-            'Magento_Checkout/js/model/payment-service': {
-                'ParadoxLabs_TokenBase/js/mixin/checkout/model/payment-service': false
-            }
-        }
     }
 };
