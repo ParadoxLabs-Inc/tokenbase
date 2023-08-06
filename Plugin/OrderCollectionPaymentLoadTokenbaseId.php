cdf16d01d81494062015804ca6cdb0c20300babc
Łukasz Bajsarowicz <lukasz@swiftotter.com>
2023-08-06 21:37:04 +0200

    #1 Prefetch Payments for Orders in Admin Grid

--- Plugin/OrderCollectionPaymentLoadTokenbaseId.php
+++ Plugin/OrderCollectionPaymentLoadTokenbaseId.php
@@ -1,19 +1,15 @@
 <?php
 /**
  * Copyright © 2015-present ParadoxLabs, Inc.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  * Need help? Try our knowledgebase and support system:
  * @link https://support.paradoxlabs.com
  */
@@ -21,8 +17,6 @@
 namespace ParadoxLabs\TokenBase\Plugin;
 
 /**
- * OrderCollectionPaymentLoadTokenbaseId Class
- *
  * Load tokenbase_id to ExtensionAttributes for order.
  */
 class OrderCollectionPaymentLoadTokenbaseId
@@ -38,44 +32,88 @@ class OrderCollectionPaymentLoadTokenbaseId
     protected $helper;
 
     /**
+     * @var \Magento\Sales\Model\ResourceModel\Order\Payment\CollectionFactory
+     */
+    private $orderPaymentCollectionFactory;
+
+    /**
      * @param \Magento\Sales\Api\Data\OrderPaymentExtensionFactory $orderPaymentExtensionFactory
      * @param \ParadoxLabs\TokenBase\Helper\Data $helper
+     * @param \Magento\Sales\Model\ResourceModel\Order\Payment\CollectionFactory $orderPaymentCollectionFactory
      */
     public function __construct(
         \Magento\Sales\Api\Data\OrderPaymentExtensionFactory $orderPaymentExtensionFactory,
-        \ParadoxLabs\TokenBase\Helper\Data $helper
+        \ParadoxLabs\TokenBase\Helper\Data $helper,
+        \Magento\Sales\Model\ResourceModel\Order\Payment\CollectionFactory $orderPaymentCollectionFactory
     ) {
         $this->orderPaymentExtensionFactory = $orderPaymentExtensionFactory;
+        $this->orderPaymentCollectionFactory = $orderPaymentCollectionFactory;
         $this->helper = $helper;
     }
 
+    public function afterLoadWithFilter(
+        \Magento\Sales\Model\ResourceModel\Order\Collection $subject,
+        \Magento\Sales\Model\ResourceModel\Order\Collection $collection
+    ): \Magento\Sales\Model\ResourceModel\Order\Collection {
+        $orderPaymentCache = $this->prefetchOrderPayments($collection);
+
+        /** @var \Magento\Sales\Model\Order $order */
+        foreach ($collection->getItems() as $order) {
+            $orderId = $order->getId();
+            $orderPayment = $orderPaymentCache[$orderId] ?? null;
+
+            if ($orderPayment && $this->isTokenbaseMethod($orderPayment)) {
+                $order->setPayment($orderPayment);
+
+                $paymentExtension = $orderPayment->getExtensionAttributes();
+                if ($paymentExtension === null) {
+                    $paymentExtension = $this->orderPaymentExtensionFactory->create();
+                }
+                $paymentExtension->setTokenbaseId($orderPayment->getData('tokenbase_id'));
+
+                $orderPayment->setExtensionAttributes($paymentExtension);
+            }
+        }
+
+        return $collection;
+    }
+
     /**
-     * @param \Magento\Sales\Api\Data\OrderSearchResultInterface $subject
-     * @param \Magento\Framework\DataObject $order
-     * @return mixed
+     * @param \Magento\Sales\Model\ResourceModel\Order\Collection $collection
+     * @return array
      */
-    public function beforeAddItem(
-        \Magento\Sales\Api\Data\OrderSearchResultInterface $subject,
-        \Magento\Framework\DataObject $order
-    ) {
-        /** @var \Magento\Sales\Api\Data\OrderInterface $order */
+    public function prefetchOrderPayments(\Magento\Sales\Model\ResourceModel\Order\Collection $collection): array
+    {
+        $orderIds = array_filter(array_unique($collection->getAllIds()));
 
-        $payment = $order->getPayment();
-        if (!($payment instanceof \Magento\Sales\Api\Data\OrderPaymentInterface)
-            || !in_array($payment->getMethod(), $this->helper->getAllMethods())) {
-            return null;
+        /** @var \Magento\Sales\Model\ResourceModel\Order\Payment\Collection $orderPaymentCollection */
+        $orderPaymentCollection = $this->orderPaymentCollectionFactory->create();
+        $orderPaymentCollection->setOrderFilter(['in' => $orderIds]);
+
+        $orderPaymentCache = [];
+        /** @var \Magento\Sales\Api\Data\OrderPaymentInterface $orderPayment */
+        foreach ($orderPaymentCollection->getItems() as $orderPayment) {
+            $orderPaymentCache[$orderPayment->getParentId()] = $orderPayment;
         }
 
-        $paymentExtension = $payment->getExtensionAttributes();
-        if ($paymentExtension === null) {
-            $paymentExtension = $this->orderPaymentExtensionFactory->create();
+        /** @var \Magento\Sales\Model\Order $order */
+        foreach ($collection->getItems() as $order) {
+            $orderId = $order->getId();
+
+            if (isset($orderPaymentCache[$orderId])) {
+                $orderPayment = $orderPaymentCache[$orderId];
+                $order->setPayment($orderPayment);
+            }
         }
-        $paymentExtension->setTokenbaseId($payment->getData('tokenbase_id'));
 
-        $payment->setExtensionAttributes($paymentExtension);
+        return $orderPaymentCache;
+    }
 
-        return [
-            $order
-        ];
+    /**
+     * @param \Magento\Framework\DataObject|\Magento\Sales\Api\Data\OrderPaymentInterface $orderPayment
+     * @return bool
+     */
+    public function isTokenbaseMethod(\Magento\Sales\Api\Data\OrderPaymentInterface $orderPayment): bool {
+        return in_array($orderPayment, $this->helper->getAllMethods());
     }
 }
